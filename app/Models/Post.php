4eea6ec520ec8cc2876d054a8ba7b38cec74c528
= <rahyandesign@gmail.com>
2025-12-26 23:11:06 +0800

    add posts crud and fix livewire problem, update category and post models to make a relationship using eloquent models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'image',
+        'title',
+        'content'
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
